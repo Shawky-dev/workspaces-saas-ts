0557d01ab800cfaac03d5d468ab91cd5bd122084
shawky-dev <shawky.projects25@gmail.com>
2026-06-02 14:11:13 +0300

    ngrok

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,4 +10,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    allowedHosts: ['indomitable-gilbert-nongelatinous.ngrok-free.dev'],
+  },
 })
